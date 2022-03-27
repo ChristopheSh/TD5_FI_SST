fa28fb1cbc57eb0b42a0548e36335aa0eb7e44ce
Christophe Shi <christophe.shi2000@gmail.com>
2022-03-27 17:37:03 +0200

    Commit Exercice2A

---
+++ ex2.py
@@ -0,0 +1,17 @@
+#Test
+
+from book import Book
+
+def main():
+    book = Book("Test")
+    book.insert_buy(10,10.0)
+    book.insert_sell(120,12.0)
+    book.insert_buy(5,10.0)
+    book.insert_buy(2,11.0)
+    book.insert_call(1,10.0)
+    book.insert_sell(10,10.0)
+
+
+if __name__ == "__main__":
+    main()
+
